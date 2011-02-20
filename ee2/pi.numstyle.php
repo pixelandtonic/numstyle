0671d6962a4b4a6582a6860b10daca9b0c9b5961
Brandon Kelly <me@brandon-kelly.com>
2011-02-20 09:11:26 -0800

    initial commit

---
+++ ee2/pi.numstyle.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('APP_VER')) exit('No direct script access allowed');
+
+
+$plugin_info = array(
+	'pi_name'        => 'Numstyle',
+	'pi_version'     => '1.0a',
+	'pi_author'      => 'Brandon Kelly',
+	'pi_author_url'  => 'http://brandon-kelly.com',
+	'pi_description' => 'Converts numbers to letters',
+	'pi_usage'       => '{exp:numstyle:upper_alpha num="1"} → A' . NL
+	                  . '{exp:numstyle:lower_alpha num="1"} → a'
+);
+
+
+/**
+ * Numstyle
+ * 
+ * @author    Brandon Kelly <brandon@pixelandtonic.com>
+ * @copyright Copyright (c) 2011 Pixel & Tonic, Inc
+ */
+class Numstyle {
+
+	/**
+	 * Constructor
+	 */
+	function __construct()
+	{
+		$this->EE =& get_instance();
+
+		$this->num = $this->EE->TMPL->fetch_param('num');
+	}
+
+	// -----------------------------------------------------------------------
+
+	private function _alpha($ascii_offset)
+	{
+		$num = $this->num - 1;
+		$alpha = '';
+
+		for ($alpha = ''; $num >= 0; $num = intval($num / 26) - 1)
+		{
+			$ascii = ($num % 26) + $ascii_offset;
+			$alpha = chr($ascii) . $alpha;
+		}
+
+		return $alpha;
+	}
+
+	/**
+	 * Upper Alpha
+	 */
+	function upper_alpha()
+	{
+		return $this->_alpha(65);
+	}
+
+	/**
+	 * Lower Alpha
+	 */
+	function lower_alpha()
+	{
+		return $this->_alpha(97);
+	}
+
+}
