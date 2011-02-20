9cd56c7fad77910c74de10be898528f72c51a124
Brandon Kelly <me@brandon-kelly.com>
2011-02-20 15:11:42 -0800

    lower_roman and upper_roman

--- ee2/pi.numstyle.php
+++ ee2/pi.numstyle.php
@@ -7,8 +7,10 @@ $plugin_info = array(
 	'pi_author'      => 'Brandon Kelly',
 	'pi_author_url'  => 'http://brandon-kelly.com',
 	'pi_description' => 'Converts numbers to letters',
-	'pi_usage'       => '{exp:numstyle:upper_alpha num="1"} → A' . NL
-	                  . '{exp:numstyle:lower_alpha num="1"} → a'
+	'pi_usage'       => '{exp:numstyle:lower_alpha num="1"} → a' . NL
+	                  . '{exp:numstyle:upper_alpha num="1"} → B' . NL
+	                  . '{exp:numstyle:lower_roman num="3"} → iii' . NL
+	                  . '{exp:numstyle:upper_roman num="4"} → IV'
 );
 
 
@@ -62,6 +64,63 @@ class Numstyle {
 	function lower_alpha()
 	{
 		return $this->_alpha(97);
+	}
+
+	// -----------------------------------------------------------------------
+
+	/**
+	 * Upper Roman
+	 */
+	function upper_roman()
+	{
+		$num = $this->num;
+		$roman = '';
+
+		$map = array(
+			array(1000000, 'M'),
+			array(900000, 'CM'),
+			array(500000, 'D'),
+			array(400000, 'CD'),
+			array(100000, 'C'),
+			array(90000, 'XC'),
+			array(50000, 'L'),
+			array(40000, 'XL'),
+			array(10000, 'X'),
+			array(9000, 'IX'),
+			array(5000, 'V'),
+			array(4000, 'IV'),
+			array(1000, 'M'),
+			array(900, 'CM'),
+			array(500, 'D'),
+			array(400, 'CD'),
+			array(100, 'C'),
+			array(90, 'XC'),
+			array(50, 'L'),
+			array(40, 'XL'),
+			array(10, 'X'),
+			array(9, 'IX'),
+			array(5, 'V'),
+			array(4, 'IV'),
+			array(1, 'I')
+		);
+
+		foreach ($map as $pair)
+		{
+			while ($num >= $pair[0]) {
+				$roman .= $pair[1];
+				$num -= $pair[0];
+			}
+		}
+
+		return $roman;
+	}
+
+	/**
+	 * Lower Roman
+	 */
+	function lower_roman()
+	{
+		return strtolower($this->upper_roman());
 	}
 
 }
